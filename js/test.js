4f114de505a7a7dfc0cbec19748eb9ec037ad4a7
acedricc <cedric.akakpo@lepoles.org>
2022-05-05 16:59:35 +0200

    0506

--- js/test.js
+++ js/test.js
@@ -1,11 +1,119 @@
+// var année=2017;
+//  var futur=3;
+//   var calcul=année+futur;
+//  document.write(calcul,"<br>"+"<br>",calcul);
 
-var nb1=10;
-var nb2=3;
-if(nb1>50){
-    nb1++;
-nb1=nb1*nb2;
- document.write("nb1 est bien égale à :" + nb1+"<br>"+nb2+"<br>");
-}
-document.write(nb1+"<br>"+nb2);
+//  var nb1,nb2,resultat;
+//  nb1=10;
+//  nb2=5;
+//  resultat=nb1+nb2;
+//  console.log(resultat);
+// document.write(resultat);
+
+// var resultat2
+// resultat2=nb1-nb2
+// console.log(resultat2);
+// document.write(resultat2);
+
+// var resultat3
+// resultat3=nb1*nb2
+// console.log(resultat3);
+// document.write(resultat3);
+
+// var resultat4
+// resultat4=nb1/nb2
+// console.log(resultat4);
+// document.write(resultat4);
+
+// var resultat5
+// resultat4=nb1%nb2
+// console.log(resultat5);
+// document.write(resultat5);
+// var nombreEntier=prompt("entrer un nombre entier","");
+// document.write(nombreEntier)
+// if (nombreEntier>10){
+//     document.write("cedric")
+// }
+// var prenom;
+//  prenom = prompt("entrez une chaine de caractere");
+// if (prenom=="cedric"){
+//     document.write(prenom);
+// }else{
+//     document.write("cette valeur n'est pas mon prenom")
+
+// }
+// var unChiffre=prompt("Entrz un Chiffre");
+// var result=unChiffre%5;
+// if(result == 0){
+//     document.write("est un multiple");
+
+// } else {
+// document.write("sa nest pas un multiple");
+// }
+// var test;
+// test=prompt("");
+// document.write=(typeof test);
+// var ced = prompt("Boite de dialogue");
+// // var cedric = 5;
+// var cedric2 = parseInt(ced);
+// if (cedric2 === 5){
+// document.write(cedric2);
+// }
 
 
+
+// var cedric
+// parseInt(cedric)
+// if(Number == 5){
+ 
+//     document.write(ced)
+// }
+// var nb1=prompt("Nombre");
+// parseFloat(nb1)
+// var nb2=nb1%13;
+// if(nb2==0){
+//  document.write("ce nombre est un multiple");
+// }
+// else {
+//  document.write("ce nombre n'est pas un multiple de 13" );
+
+// }
+
+
+// var saisie=prompt("");
+// var resultat=parseFloat(saisie);
+// if(isNaN(resultat)){
+//     document.write(saisie+"est une chaine de caractere peut-etre composée de chiffres");
+
+// }else{
+//     var finalResalt = resultat %7;
+//     if(finalResalt == 0 ){
+//         document.write(saisie+"est un multiple de 7");
+//     }else{
+//         document.write(saisie+"n'est pas un multiple de 7 et le résultat du modulo est:"+finalResalt);
+//     }
+// }
+
+// var nombre=prompt("Entrez un nombre");
+//  var nombreMultiplier=(nombre*1.8+32);
+//  document.write( nombreMultiplier+"<br>");
+//  var modulo = nombreMultiplier%3;
+//  document.write(modulo);
+
+// boite1 = 40, 
+// boite2 = 50
+// boite3 = 60
+// var boite1 ,boite2,boite3;
+// boite3 = boite1;
+// boite1 = boite2;
+// boite2 = boite3;
+// console.log(boite1)
+// console.log(boite2)
+// prompt("",boite2)
+// prompt("",boite1)
+
+// for (var i = 25; i<=35;i+=2){
+//     document.write("<p>Instruction exécutée:"+i+"</p>");
+// }
+
+var nb1=
